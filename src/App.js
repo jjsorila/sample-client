f303886df4000a52e6f45bd9472a2f947b6b4601
JJS <jjsorila@gmail.com>
2022-01-22 13:42:31 +0800

    init repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const host = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://18.212.177.98"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [name, setName] = useState('World')
+
+  const getName = async() => {
+    const data = await fetch(`${host}/api`).then(res => res.json())
+    setName(data.name)
+  }
+
+  return <div>
+    <h1>Hello {name}</h1>
+    <button onClick={getName}>Get Name</button>
+  </div>;
+};
 
 export default App;
